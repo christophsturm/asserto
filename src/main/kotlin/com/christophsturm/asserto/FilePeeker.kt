2cbe401e2ae252b7bcfbc1f7de1533d75e62ad37
Christoph Sturm <me@christophsturm.com>
2018-02-25 13:55:25 +0100

    simplify by using substringBefore

--- src/main/kotlin/com/christophsturm/asserto/FilePeeker.kt
+++ src/main/kotlin/com/christophsturm/asserto/FilePeeker.kt
@@ -9,15 +9,11 @@ object FilePeeker {
     fun  getFileInfo(offset: Int = 2): FileInfo {
         val e = RuntimeException()
         val entry = e.stackTrace[offset]
-        val className = entry.className
-        val indexOfDollar = className.indexOf('$')
-        val realClassName = if (indexOfDollar != -1) {
-            className.substring(0, indexOfDollar)
-        } else className
-        val clazz = javaClass.classLoader.loadClass(realClassName)!!
+        val className = entry.className.substringBefore('$')
+        val clazz = javaClass.classLoader.loadClass(className)!!
         val classFileAbsolutePath = File(clazz.protectionDomain.codeSource.location.path).absolutePath
         val buildDir = if (classFileAbsolutePath.contains("/out/")) "out/test/classes" else if (classFileAbsolutePath.contains("build/classes/java")) "build/classes/java/test" else "build/classes/test"
-        val baseName = classFileAbsolutePath.replace(buildDir, "src/test/kotlin").plus("/" + realClassName.replace(".", "/"))
+        val baseName = classFileAbsolutePath.replace(buildDir, "src/test/kotlin").plus("/" + className.replace(".", "/"))
         val sourceFileName = if (File(baseName.plus(".kt")).exists()) baseName.plus(".kt") else baseName.plus(".java").replace("src/test/kotlin", "src/test/java")
         val line = FileReader(sourceFileName).useLines {
             it.drop(entry.lineNumber-1).first()
